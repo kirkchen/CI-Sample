a11026c5fa2ce6716ca0d7cf8739b1bc5af3607a
kirkchen <rwk0119@yahoo.com.tw>
2012-10-27 13:30:00 +0800

    add document header

--- CI-Sample/IPriceDiscountService.cs
+++ CI-Sample/IPriceDiscountService.cs
@@ -2,10 +2,23 @@
 
 namespace CI_Sample
 {
+    /// <summary>
+    /// IPriceDiscountService
+    /// </summary>
     public interface IPriceDiscountService
     {
+        /// <summary>
+        /// Calculates the specified total price.
+        /// </summary>
+        /// <param name="totalPrice">The total price.</param>
+        /// <returns>Result</returns>
         double Calculate(double totalPrice);
 
+        /// <summary>
+        /// Duplicates the calculate.
+        /// </summary>
+        /// <param name="totalPrice">The total price.</param>
+        /// <returns>Result</returns>
         double DuplicateCalculate(double totalPrice);
     }
 }
