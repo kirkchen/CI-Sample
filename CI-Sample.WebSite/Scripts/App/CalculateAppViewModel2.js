2d2aa8712ebeebb32cdb7246c91a3add87f31a9a
kirkchen <rwk0119@yahoo.com.tw>
2012-10-28 10:57:02 +0800

    add file

---
+++ CI-Sample.WebSite/Scripts/App/CalculateAppViewModel2.js
@@ -0,0 +1,19 @@
+﻿(function ($, ko) {
+    var appViewModel = {
+        TotalPrice: ko.observable(0),
+        DiscountPrice: ko.observable(0),
+        CalculateDiscountPrice: function () {
+            var self = this;
+            $.ajax({
+                url: "/Calculate/CalculateDiscount",
+                type: "POST",
+                data: { totalPrice: this.TotalPrice() },
+                success: function (response) {
+                    self.DiscountPrice(response.DiscountPrice);
+                }
+            });
+        }
+    }
+
+    ko.applyBindings(appViewModel);
+})($, ko);
