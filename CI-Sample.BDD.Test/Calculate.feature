940f748d6bf568fd5f2a357e3c220ca7e6c5c3f0
kirkchen <kirkchen@nine-yi.com>
2013-08-23 16:23:35 +0800

    Add BDD.Test

---
+++ CI-Sample.BDD.Test/Calculate.feature
@@ -0,0 +1,19 @@
+﻿Feature: Calculate
+	In order to get discounted price
+	As a clerk
+	I want to enter the total price	
+
+Scenario: Total price over 100 dollar, get 20% discount
+	Given total price as 120 dollar	
+	When do caculation
+	Then discounted price is 96 dollar
+
+Scenario: Total price between 20 dollar and 100 dollar, get 10% discount
+	Given total price as 80 dollar	
+	When do caculation
+	Then discounted price is 72 dollar
+
+Scenario: Total price less than 20 dollar, get no discount
+	Given total price as 10 dollar	
+	When do caculation
+	Then discounted price is 10 dollar
---
+++ CI-Sample.BDD.Test/StepDefinitions/CalculateSteps.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TechTalk.SpecFlow;
+
+namespace CI_Sample.BDD.Test.StepDefinitions
+{
+    [Binding]
+    public class CalculateSteps
+    {
+        private double totalPrice;
+
+        private double discountedPrice;
+
+        private PriceDiscountService priceDiscountService;
+
+        [Given(@"total price as (.*) dollar")]
+        public void GivenTotalPriceAsDollar(double totalPrice)
+        {
+            this.totalPrice = totalPrice;
+
+            this.priceDiscountService = new PriceDiscountService();
+        }
+        
+        [When(@"do caculation")]
+        public void WhenDoCaculation()
+        {
+            this.discountedPrice = this.priceDiscountService.Calculate(this.totalPrice);
+        }
+        
+        [Then(@"discounted price is (.*) dollar")]
+        public void ThenDiscountedPriceIsDollar(double discountedPrice)
+        {
+            Assert.AreEqual(this.discountedPrice, discountedPrice);
+        }
+    }
+}
